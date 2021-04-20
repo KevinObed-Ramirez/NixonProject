e82954a68b3da922069a3e5df17ca6a72ba357e2
Kevin Ramirez <kramirez234@west-mec.org>
2021-04-19 18:21:12 -0700

    Added files and worked 4/19

---
+++ index.js
@@ -0,0 +1,10 @@
+
+window.onload = (event) => {
+    confirm("Page Loaded: Initiate Scam?");
+};
+
+function nixon() {
+    document.getElementById("nixon").onload = (event) => {
+        
+    }
+};
