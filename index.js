2e2f81e06e0f4e6bae727e527ce01b4f533f2f17
Kevin Ramirez <kramirez234@west-mec.org>
2021-04-20 18:24:45 -0700

    Done for today 4/20

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ window.onload = (event) => {
 };
 
 function nixon() {
-    document.getElementById("nixon").onload = (event) => {
-        
+    window.alert("HOORAY YOU PICKED THE RIGHT CANDIDATE")
+     window.open("Morgana Fortnite.mp4", "Congrats!", "width=1500,height=1500");   // Opens a new window
+};
+
+
+// TURN DOWN VOLUME FOR WRONG FUNCTION
+function wrong(){
+    window.alert("WRONG ANSWER! LOWER VOLUME")
+    var i =0;
+    var stop = 150;
+    for (i=0;i<150; i++){
+        document.write('<video width="100" height="100" controls autoplay><source src="Your computer has virus.mp4" type="video/mp4"><source src="Your Computer has virus.mp4" type="video/ogg">Your browser does not support the video tag.</video>');
     }
 };
